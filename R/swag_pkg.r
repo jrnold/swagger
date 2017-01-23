67883f664ab36de8767cddbe0e6f5557c70bfd66
jrnold <jeffrey.arnold@gmail.com>
2017-01-22 18:12:12 -0800

    Fix roxygen bug that generated grape-s.Rd
    Roxygen was creating man/grape-s.Rd because it was interpreting the string template to generate Roxygen documentation as documentation for this package.

--- R/swag_pkg.r
+++ R/swag_pkg.r
@@ -4,16 +4,18 @@ swag_pkg <- function(g) {
 
   g$RSWAG$PKGR <- paste(g$RSWAG$pkg_dir, "R", sprintf("%s-package.r", g$RSWAG$pkg_name), sep="/")
 
-  pkgr <- "#' %s
-#'
-#' %s
-#'
-#' @name %s
-#' @docType package
-#' @author Jane Doe
-#' @import httr
-NULL
-"
+  # if this is a single string it may be interpreted as actual
+  # documentation by Roxygen
+  pkgr <- paste0(c("#' %s",
+            "#'",
+            "#' %s",
+            "#'",
+            "#' @name %s",
+            "#' @docType package",
+            "#' @author Jane Doe",
+            "#' @import httr",
+            "NULL"), collapse = "\n")
+
   writeLines(sprintf(pkgr, g$info$title, g$info$description, g$RSWAG$pkg_name),
              g$RSWAG$PKGR)
 
